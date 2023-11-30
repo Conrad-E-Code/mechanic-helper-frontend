4ad4081d0abfb064a80cd332de9d7289e8a9d98b
Conrad Etherington <conrad.etherington@gmail.com>
2023-11-29 22:02:37 -0700

    first commit

---
+++ src/components/IssueForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const IssueForm = () => {
+  return (
+    <div>
+        <form >
+            <label> Car <input /></label>
+        </form>
+    </div>
+  )
+}
+
+export default IssueForm
